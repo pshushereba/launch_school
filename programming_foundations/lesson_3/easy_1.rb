a844db44ae4f62ec38035f035df9112dd39763d9
Patrick Shushereba <pshushereba@gmail.com>
2016-12-24 17:12:56 -0800

    Starting Lesson 3

---
+++ programming_foundations/lesson_3/easy_1.rb
@@ -0,0 +1,13 @@
+# Question 1
+
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+# It will print out all the items in the numbers array. The uniq
+# method without the bang operator does not modify the
+# original array.
+
+# Question 2
+
